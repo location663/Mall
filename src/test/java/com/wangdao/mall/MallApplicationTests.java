8a2afd027a8c9da59b3026d386df81033674151a
lxmxiao <397115709@qq.com>
2019-11-18 22:30:15 +0800

    lxmxiao_微信首页

--- src/test/java/com/wangdao/mall/MallApplicationTests.java
+++ src/test/java/com/wangdao/mall/MallApplicationTests.java
@@ -1,21 +1,2 @@
-package com.wangdao.mall;
 
-import com.wangdao.mall.bean.AdminDO;
-import com.wangdao.mall.service.admin.AdminService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.Map;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
-
-@SpringBootTest
-class MallApplicationTests {
-
-    @Autowired
-    AdminService adminService;
-
-}
 
