dfb34b25e8e11d6337698fa3dba469c6dc78be56
location663 <463818914@qq.com>
2019-11-20 22:04:03 +0800

    a

--- src/main/java/com/wangdao/mall/exception/MallExceptionHandler.java
+++ src/main/java/com/wangdao/mall/exception/MallExceptionHandler.java
@@ -11,18 +11,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class MallExceptionHandler {
 
     @ExceptionHandler(NoSuchGoodsException.class)
-    public BaseReqVo noSuchGoodsHandler(NoSuchGoodsException e){
+    public BaseReqVo noSuchGoodsHandler(){
         return new BaseReqVo(null , null, 702);
     }
 
 
     @ExceptionHandler(InvalidFormatException.class)
-    public BaseReqVo invalidFormat(InvalidFormatException e){
+    public BaseReqVo invalidFormat(){
         return new BaseReqVo(null , "您输入有误，请重新输入", 601);
     }
 
     @ExceptionHandler(WxException.class)
-    public BaseReqVo wxHandler(NoSuchGoodsException e){
+    public BaseReqVo wxHandler(WxException e){
         return new BaseReqVo(null , e.getMessage(), 702);
     }
 
